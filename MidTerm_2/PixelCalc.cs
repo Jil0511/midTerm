c7cb0075abee830a33c08709f7ee479a8f87958e
Jil0511 <jpatel5062@conestogac.on.ca>
2023-06-22 11:34:25 -0400

    Reference added

---
+++ MidTerm_2/PixelCalc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_2
+{
+    public class PixelCalc
+    {
+        public double screenWidth { get; set; }
+        public double screenHeight { get; set; }
+        public double daigonalSize { get; set; }
+
+        public void calculator()
+        {
+            Console.WriteLine("Enter screen width: ");
+            double screenWidth;
+            if (!double.TryParse(Console.ReadLine(), out screenWidth))
+            {
+                Console.WriteLine("Enter Correct Input");
+                return;
+            }
+            Console.WriteLine("Enter screen height: ");
+            double screenHeight;
+            if (!double.TryParse(Console.ReadLine(), out screenHeight))
+            {
+                Console.WriteLine("Enter Correct Input");
+                return;
+            }
+
+            Console.WriteLine("Enter daigonal size:");
+            double daigonalSize;
+            if (!double.TryParse(Console.ReadLine(), out daigonalSize))
+            {
+                Console.WriteLine("Enter Correct Input");
+                return;
+            }
+
+            double PPI = Math.Sqrt(Math.Pow(screenWidth, 2) + Math.Pow(screenHeight, 2)) / daigonalSize;
+            double diagonalPixels = Math.Sqrt(Math.Pow(screenWidth, 2) + Math.Pow(screenHeight, 2));
+            double dotPitch = diagonalPixels / PPI;
+
+            Console.WriteLine("PPI: " + PPI);
+            Console.WriteLine("Diagonal (in pixels): " + diagonalPixels);
+            Console.WriteLine("Dot pitch: " + dotPitch);
+
+        }
+    }
+}
---
+++ MidTerm_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Enter Screen dimensions");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("Select one option: ");
+                string input = Console.ReadLine();
+                PixelCalc pixelCalc = new PixelCalc();
+                if (input == "1" || input == "2")
+                {
+                    switch (input)
+                    {
+                        case "1":
+                            pixelCalc.calculator();
+                            break;
+
+                        case "2":
+                            System.Environment.Exit(0);
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+    }
+}
+
